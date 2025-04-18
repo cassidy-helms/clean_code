7b8581b8f0672a7d6089cff257d74a00b6a3a5bd
Cassidy Helms <cassidy.helms@gmail.com>
2025-04-18 11:34:15 -0400

    added rough draft args code pulled from clean code

---
+++ src/main/java/com/clean_code/App.java
@@ -0,0 +1,29 @@
+package com.clean_code;
+
+import java.text.ParseException;
+
+import com.clean_code.args_rough_draft.Args;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+        try {
+            String[] fakeArgs = { "-l", "-p", "8080", "-d", "C:/home/code" };
+            Args arg = new Args("l,p#,d*", fakeArgs);
+            boolean logging = arg.getBoolean('l');
+            int port = arg.getInt('p');
+            String directory = arg.getString('d');
+            System.out.println("Logging: " + logging);
+            System.out.println("Port: " + port);
+            System.out.println("Directory: " + directory);
+        } catch(ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
