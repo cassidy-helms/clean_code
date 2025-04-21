4e04361bd06ba305d96810ad6aeb2a9b99f38fdc
Cassidy Helms <cassidy.helms@gmail.com>
2025-04-21 11:18:19 -0400

    move null check to top of setArgument

--- src/main/java/com/clean_code/args_refactor_2/Args.java
+++ src/main/java/com/clean_code/args_refactor_2/Args.java
@@ -120,6 +120,9 @@ public class Args {
     private boolean setArgument(char argChar) throws ArgsException {
         ArgumentMarshaler m = marshalers.get(argChar);
 
+        if(m == null) 
+            return false;
+
         try {
             if(m instanceof BooleanArgumentMarshaler)
                 setBooleanArg(m);
@@ -127,8 +130,6 @@ public class Args {
                 setStringArg(m);
             else if(m instanceof IntegerArgumentMarshaler)
                 setIntArg(m);
-            else
-                return false;
         } catch(ArgsException e) {
             valid = false;
             errorArgumentId = argChar;
