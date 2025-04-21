5fdf9e2a0f169ad2b40c5e16fbd1d5cec1d6059b
Cassidy Helms <cassidy.helms@gmail.com>
2025-04-21 12:00:32 -0400

    move ArgsException to separate class

---
+++ src/main/java/com/clean_code/args_refactor_3/exception/ArgsException.java
@@ -0,0 +1,15 @@
+package com.clean_code.args_refactor_3.exception;
+
+public class ArgsException extends Exception {
+    private char errorArgumentId = '\0';
+    private String errorParameter = "TILT";
+    private ErrorCode errorCode = ErrorCode.OK;
+
+    public ArgsException() {}
+
+    public ArgsException(String message) { super(message); }
+
+    public enum ErrorCode {
+        OK, MISSING_STRING, MISSING_INTEGER, INVALID_INTEGER, MISSING_DOUBLE, INVALID_DOUBLE, UNEXPECTED_ARGUMENT
+    }
+};
