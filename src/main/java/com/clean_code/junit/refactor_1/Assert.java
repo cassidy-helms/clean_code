688e4862fc0ae3dec2745f1893a9fd0a1619c3a6
Cassidy Helms <cassidy.helms@gmail.com>
2025-04-24 13:57:53 -0400

    setup for refactor 1

---
+++ src/main/java/com/clean_code/junit/refactor_1/Assert.java
@@ -0,0 +1,15 @@
+package com.clean_code.junit.refactor_1;
+
+public class Assert {
+    public static String format(String message, String expected, String actual) {
+        StringBuilder sb = new StringBuilder();
+
+        if(message != null) {
+            sb.append(message + " ");
+        }
+
+        sb.append("expected:<" + expected + "> but was:<" + actual + ">");
+
+        return sb.toString();
+    }
+}
